5c964119d3ac9fee332358b427fd8cce720defcb
chensation <32966177+chensation@users.noreply.github.com>
2021-04-21 11:39:13 -0400

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+
+    public bool hideCursor = false;
+
+	public List<GameObject> temples;
+
+	public int currentTeleport = -1;
+
+    private void Start()
+    {
+        Cursor.visible = !hideCursor;
+    }
+
+    public void TeleportToTemple()
+    {
+        Transform player = GameObject.FindWithTag("Player").transform;
+
+        foreach (Transform child in temples[currentTeleport].transform)
+        {
+            if (child.CompareTag("Teleport"))
+            {
+                player.position = child.position;
+                break;
+            }
+        }
+        temples[currentTeleport].GetComponent<Temple>().OpenDoor();
+
+    }
+}
